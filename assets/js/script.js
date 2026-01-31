fbe2e71459d5edcc5395cf4fe95572d7160c4e64
Yogesh Mapari <patilmapari@gmail.com>
2026-01-31 20:17:20 +0530

    added changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,14 +144,16 @@ const pages = document.querySelectorAll("[data-page]");
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
+    const clickedText = this.innerHTML.toLowerCase().trim();
+
+    for (let j = 0; j < pages.length; j++) {
+      if (clickedText === pages[j].dataset.page) {
+        pages[j].classList.add("active");
+        navigationLinks[j].classList.add("active");
         window.scrollTo(0, 0);
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        pages[j].classList.remove("active");
+        navigationLinks[j].classList.remove("active");
       }
     }
 
